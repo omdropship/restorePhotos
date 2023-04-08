b4ac35f9dfab90e09da57961bd012615a8b473d2
omdropship <86162902+omdropship@users.noreply.github.com>
2023-04-09 00:21:11 +0700

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,8 +7,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID || "297668138985-3dct069mdarqo65n6gtjb46bh4rbn9fu.apps.googleusercontent.com",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "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",
     }),
   ],
 };
